bbddd5bf9c99852f131ce3173216ee4ece3ddc9e
kyle wilichowski <kyle.wilichowski@jbhunt.com>
2024-06-05 23:12:06 -0500

    fix vercel.json

--- api/checkEvents.js
+++ api/checkEvents.js
@@ -1,39 +1,35 @@
 const axios = require('axios');
-const sgMail = require('@sendgrid/mail');
+const nodemailer = require('nodemailer');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 module.exports = async (req, res) => {
   try {
-    const response = await axios.get(
-      'https://comedy-mothership-api.vercel.app/2024-08-30/2024-09-02'
-    );
+    const response = await axios.get('https://comedy-mothership-api.vercel.app/2024-08-30/2024-09-07');
 
     if (response.data.length > 0) {
-      const msg = {
-        to: 'wilichowskikyle@gmail.com',
-        from: 'wilichowskikyle@gmail.com',
-        subject: 'New Shows Found',
-        text: `New shows found: ${JSON.stringify(response.data)}`,
-        html: `<strong>New shows found:</strong><br>${JSON.stringify(
-          response.data,
-          null,
-          2
-        )}`,
+      const mailOptions = {
+          from: process.env.EMAIL_USER,
+          to: 'wilichowskikyle@gmail.com',
+          subject: 'New Shows Found',
+          text: `New shows found: ${JSON.stringify(response.data)}`,
+          html: `<strong>New shows found:</strong><br>${JSON.stringify(response.data, null, 2)}`
       };
 
-      await sgMail.send(msg).then((response) => {
-          console.log(response[0].statusCode)
-          console.log(response[0].headers)
-        })
-        .catch((error) => {
-          console.error(error)
-        });
+      // await transporter.sendMail(mailOptions);
     }
 
     res.status(200).json({ success: true });
   } catch (error) {
     console.error('Error checking scraper API:', error);
-    res.status(500).json({ error: 'Failed to check scraper API' });
+    res.status(500).json({ error: 'Failed to check dates or send email' });
   }
 };
