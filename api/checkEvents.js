7ffdc762d6eb6b2f3223be0add1b6f6a01f544b7
kyle wilichowski <kyle.wilichowski@jbhunt.com>
2024-06-05 23:50:27 -0500

    add cronjob action and nodemailer

--- api/checkEvents.js
+++ api/checkEvents.js
@@ -2,34 +2,35 @@ const axios = require('axios');
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false,
+  host: 'smtp.zoho.com',
+  port: 465,
+  secure: true, //ssl
   auth: {
-    user: process.env.EMAIL_USER,
+    user: process.env.EMAIL_ADDRESS,
     pass: process.env.EMAIL_PASS,
   },
 });
 
 module.exports = async (req, res) => {
   try {
-    const response = await axios.get('https://comedy-mothership-api.vercel.app/2024-08-30/2024-09-07');
+    const dateRange = '2024-08-30/2024-09-02';
+    const response = await axios.get('https://comedy-mothership-api.vercel.app/' + dateRange);
 
     if (response.data.length > 0) {
       const mailOptions = {
-          from: process.env.EMAIL_USER,
+          from: process.env.EMAIL_ADDRESS,
           to: 'wilichowskikyle@gmail.com',
-          subject: 'New Shows Found',
-          text: `New shows found: ${JSON.stringify(response.data)}`,
-          html: `<strong>New shows found:</strong><br>${JSON.stringify(response.data, null, 2)}`
+          subject: `New shows found for ${dateRange.replace('/', ' to ')}`,
+          text: `New shows found for ${dateRange.replace('/', ' to ')}`,
+          html: `<h2>New shows found:</h2>${response.data.map(event => `<h3>${event.title}</h3><h5>Date: ${event.start.split('T')[0]}</h5><a href="${event.url}">Buy here</a><hr><br>`).join('\n')}`
       };
 
-      // await transporter.sendMail(mailOptions);
+      await transporter.sendMail(mailOptions).then(console.log);
     }
 
     res.status(200).json({ success: true });
   } catch (error) {
-    console.error('Error checking scraper API:', error);
-    res.status(500).json({ error: 'Failed to check dates or send email' });
+    console.error('Error checking events or sending email:', error);
+    res.status(500).json({ error: 'Failed to check events or send email' });
   }
 };
