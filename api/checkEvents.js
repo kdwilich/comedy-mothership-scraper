6b0c0df5725134e5e555efe5faaec546e9fe7789
kyle wilichowski <kyle.wilichowski@jbhunt.com>
2024-06-25 13:00:17 -0500

    update date range to 09-01 and add email

--- api/checkEvents.js
+++ api/checkEvents.js
@@ -13,13 +13,13 @@ const transporter = nodemailer.createTransport({
 
 module.exports = async (req, res) => {
   try {
-    const dateRange = '2024-08-30/2024-09-02';
+    const dateRange = '2024-08-30/2024-09-01';
     const response = await axios.get('https://comedy-mothership-api.vercel.app/' + dateRange);
 
     if (response.data.length > 0) {
       const mailOptions = {
           from: process.env.EMAIL_ADDRESS,
-          to: 'wilichowskikyle@gmail.com',
+          to: ['wilichowskikyle@gmail.com','biskerton07@gmail.com'],
           subject: `New shows found for ${dateRange.replace('/', ' to ')}`,
           text: `New shows found for ${dateRange.replace('/', ' to ')}`,
           html: `<h2>New shows found:</h2>${response.data.map(event => `<h3>${event.title}</h3><h5>Date: ${event.start.split('T')[0]}</h5><a href="${event.url}">Buy here</a><hr><br>`).join('\n')}`
